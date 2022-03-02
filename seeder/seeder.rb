c61e81629116905b20061d89e2ee332a505ce1c5
yastamardika <rattyasta1@gmail.com>
2022-03-03 04:09:04 +0700

    fix fetch type browser on seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,7 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    fetch_type: 'browser',
+    fetch_type: "browser",
     url: "https://www.aliexpress.com/category/100003109/women-clothing.html",
     vars: {
       category: "Women's clothing"
