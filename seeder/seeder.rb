9d96ffdfe55693f2887edd19353751468ae83d22
yastamardika <rattyasta1@gmail.com>
2022-03-03 03:29:13 +0700

    remove header on seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,6 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
     url: "https://www.aliexpress.com/category/100003109/women-clothing.html",
     vars: {
       category: "Women's clothing"
