5e18d0701f04dbc1af5978e72fd1fb4651a771c6
yastamardika <rattyasta1@gmail.com>
2022-03-04 03:46:56 +0700

    fix pagination

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -2,9 +2,7 @@ html = Nokogiri.HTML(content)
 
 #load products
 products = html.css('.JIIxO ._3t7zg')
-# p products
 products.each do |product|
-  # a_element = product.at_css('a._3t7zg')
   url = URI.join('https://aliexpress.com', product['href']).to_s.split('?').first
   if product
     if url =~ /\Ahttps?:\/\//i
@@ -22,19 +20,31 @@ products.each do |product|
   end
 end
 
-#load paginated links
-pagination_links = html.css('.next-pagination-list')
-p pagination_links
-# pagination_links.each do |link|
-#   p link
-#   l_val = link.text.strip
-#   if l_val !~ /next|previous/i && l_val.to_i < 11 #limit pagination to 10 pages
-#     url = URI.join('https:', link['href']).to_s.split('?').first
-#     pages << {
-#         url: url,
-#         page_num: l_val,
-#         page_type: 'listings',
-#         fetch_type: 'browser',
-#       }
-#   end
-# end
+index = page["vars"]["page"]
+next_url = page["vars"]["base_url"] + "?page=#{index}" if index < 6
+index += 1
+if index < 8
+  pages << {
+    page_type: "listings",
+    method: "GET",
+    fetch_type: "browser",
+    url: ,
+    headers: {
+      "User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+      "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'"
+    },
+    vars: {
+      category: "Women's clothing",
+      "base_url" => "https://www.aliexpress.com/category/100003109/women-clothing.html",
+      "page" => index
+    },
+    driver: {
+      "code": "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
+    }
+  }
+end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,11 +3,21 @@ pages << {
     method: "GET",
     fetch_type: "browser",
     url: "https://www.aliexpress.com/category/100003109/women-clothing.html?page=1",
-    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    headers: {
+      "User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+      "Sec-CH-UA" => "'Not A;Brand';v='99', 'Chromium';v='96', 'Google Chrome';v='96'"
+    },
     vars: {
-      category: "Women's clothing"
+      category: "Women's clothing",
+      "base_url" => "https://www.aliexpress.com/category/100003109/women-clothing.html",
+      "page" => 2
     },
     driver: {
-      "code": "await page.evaluate('window.scrollBy(0,3600)'); await sleep(1000);"
+      "code": "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+      await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
     }
   }
