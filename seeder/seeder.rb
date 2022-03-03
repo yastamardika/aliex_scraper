cc8edf4638343dda0b084b03091bf67452f07db5
yastamardika <rattyasta1@gmail.com>
2022-03-04 01:04:09 +0700

    add headers

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,10 +3,11 @@ pages << {
     method: "GET",
     fetch_type: "browser",
     url: "https://www.aliexpress.com/category/100003109/women-clothing.html",
+    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
     vars: {
       category: "Women's clothing"
     },
     driver: {
-      "code": "await page.evaluate('window.scrollBy(0,3600)'); await sleep(1000);"
+      "code": "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
     }
   }
